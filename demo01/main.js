b6866b42caf9aa7e93c4acf96668e57c8c7e425c
bjoha013 <63038377+bjoha013@users.noreply.github.com>
2020-08-31 12:17:27 +0300

    Update main.js
    johanna kommentoi

--- demo01/main.js
+++ demo01/main.js
@@ -10,7 +10,7 @@ const serveri = http.createServer((req, res) => {
         let tiedot = url.parse(req.url, true).query;
     
         let nimi = (tiedot.nimi) ? tiedot.nimi : "tuntematon";
-    
+    //200 = kaikki on ok, 401 forbidden 402 not found jne.
         res.writeHead(200, { "Content-type" : "text/html; charset=utf-8" });
         res.write(`<h1>Demo 1: Node.js -palvelinohjelmoinnin perusteita</h1>`);
         res.write(`<h2>Heippa, ${nimi}!</h2>`);
